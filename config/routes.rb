dda2c50195366db17ac5fbb4bf0d22a6c88a8e9e
Greg Smith <greg_smith1992@hotmail.com>
2019-05-10 16:50:51 +0100

    Added methods for CRUDing my tasks on rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  # READ
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  delete 'tasks/:id', to: 'tasks#destroy'
+  # CREATE
+  # Request to GET the form
+  # Request to SUBMIT the form
+  post 'tasks', to: 'tasks#create'
+
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch 'tasks/:id', to: 'tasks#update'
+
+
+  # resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
